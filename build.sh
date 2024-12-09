205c3674290a98a7e6a13d0bb933cc185c95bb85
Dave Corley <smokydave@icloud.com>
2024-12-09 12:22:44 -0700

    FEAT: Add a target for windows jobs and hope to god they work

--- build.sh
+++ build.sh
@@ -4,12 +4,19 @@ set -euo
 
 PYCURL_URL="${1:-https://files.pythonhosted.org/packages/64/d2/a4c45953aed86f5a0c9717421dd725ec61acecd63777dd71dfe3d50d3e16/pycurl-7.45.3-cp310-cp310-manylinux_2_28_x86_64.whl}"
 TARGET_TRIPLE="${2:-x86_64-unknown-linux-gnu}"
+TARGET_OS="${3:-linux}"
 
 rm -rf dep build
 mkdir -p oxcache
 
 wget -O pycurl.zip $PYCURL_URL
-mkdir -p dep/bin && unzip -j pycurl.zip "pycurl.libs/*" -d dep/bin/
+mkdir -p dep/bin
+
+if [ "$TARGET_OS" = "linux" ]; then
+    unzip -j pycurl.zip "pycurl.libs/*" -d dep/bin/
+else
+    unzip -j pycurl.zip "pycurl-7.45.3.data/platlib/*" -d dep/bin/
+fi
 
 if [ -n "${USE_VENV+x}" ]; then
     python -m venv .venv
