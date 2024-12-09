c24aab4bf12ad1fe62133fabd2acb4e5d7887f84
Dave Corley <smokydave@icloud.com>
2024-12-09 10:35:02 -0700

    FIX: Upload umo binary, not the source dir

--- build.sh
+++ build.sh
@@ -7,5 +7,5 @@ wget -O pycurl.zip $PYCURL_URL
 mkdir -p dep/bin && unzip -j pycurl.zip "pycurl.libs/*" -d dep/bin/
 python -m pip install pyoxidizer
 pyoxidizer run --target install --release
-mv build/$TARGET_TRIPLE/release/install/umo .
+mv build/$TARGET_TRIPLE/release/install/umo ./umo-$TARGET_TRIPLE
 mv build/$TARGET_TRIPLE/release/install/lib dep/
