c264102d50def3bfa879a3ddcb9a988ae6b5351d
Dave Corley <smokydave@icloud.com>
2024-12-09 10:43:37 -0700

    CLEANUP: Ask it nicely not to use notifications so it doesn't throw in CI

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ TARGET_TRIPLE="$2"
 wget -O pycurl.zip $PYCURL_URL
 mkdir -p dep/bin && unzip -j pycurl.zip "pycurl.libs/*" -d dep/bin/
 python -m pip install pyoxidizer
-pyoxidizer run --target install --release
+UMO_NO_NOTIFICATIONS="pls" pyoxidizer run --target install --release
 mv build/$TARGET_TRIPLE/release/install/umo ./umo-$TARGET_TRIPLE
 mv build/$TARGET_TRIPLE/release/install/lib dep/
